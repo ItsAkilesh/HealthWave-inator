86283311c44e30af0e61e953deb6898714b223b4
ItsAkilesh <akilesh0876@gmail.com>
2023-07-09 11:44:04 +0530

    Added continuous chat feature

--- app/src/main/java/com/baymax/ai/ui/chat/ChatFragment.java
+++ app/src/main/java/com/baymax/ai/ui/chat/ChatFragment.java
@@ -7,14 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.baymax.ai.R;
 import com.baymax.ai.databinding.FragmentChatBinding;
@@ -24,38 +24,43 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class ChatFragment extends Fragment {
 
     private FragmentChatBinding binding;
-    public TextView user_input,gpt_out;
-    public EditText input;
+     public EditText input;
     public Button send;
+    public ListView list;
+    public ArrayList<String> history;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                 .permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        ChatViewModel chatViewModel =
-
-                new ViewModelProvider(this).get(ChatViewModel.class);
 
         binding = FragmentChatBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        user_input = (TextView) root.findViewById(R.id.user_input);
-        gpt_out=(TextView) root.findViewById(R.id.gpt_out);
-        input = (EditText) root.findViewById(R.id.input);
-        send = (Button) root.findViewById(R.id.send);
+
+        input = root.findViewById(R.id.input);
+        send = root.findViewById(R.id.send);
+        list = root.findViewById(R.id.list);
+        history = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, history);
+        list.setAdapter(adapter);
+
 
         send.setOnClickListener(new View.OnClickListener() {
             private static final String FLASK_ENDPOINT = "http://192.168.1.100:5000/askgpt";
             @Override
             public void onClick(View v) {
                 if (input!=null) {
-                    String query = input.getText().toString();
+                    String query =  input.getText().toString();
+                    String prompt = String.join("\n", history)  + query;
                     String gen_message;
-                    user_input.setText(query);
+                    history.add(query);
+                    adapter.notifyDataSetChanged();
                     InputMethodManager inputManager = (InputMethodManager) root.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
                     inputManager.hideSoftInputFromWindow(root.getWindowToken(), 0);
                     input.setText("");
@@ -67,7 +72,7 @@ public class ChatFragment extends Fragment {
                         connection.setDoOutput(true);
 
                         // Construct the request body
-                        String requestBody = "Message=" + query;
+                        String requestBody = "Message=" + prompt;
 
                         OutputStream outputStream = connection.getOutputStream();
                         outputStream.write(requestBody.getBytes());
@@ -85,7 +90,7 @@ public class ChatFragment extends Fragment {
                             reader.close();
 
                             // Extract the generated message from the JSON response
-                            gen_message = response.toString().substring(12, (response.length()-2));
+                            gen_message = response.substring(12, (response.length()-2));
                         } else {
                             gen_message = "API request failed with response code: " + responseCode;
                         }
@@ -94,10 +99,11 @@ public class ChatFragment extends Fragment {
                     } catch (Exception e) {
                         e.printStackTrace();
                         gen_message = "API request failed: " + e.getMessage();
-                        gpt_out.setText("");
+
                     }
 
-                    gpt_out.setText(gen_message);
+                   history.add(gen_message);
+                    adapter.notifyDataSetChanged();
 
                 }
                 else
